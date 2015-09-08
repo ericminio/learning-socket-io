52412a2c84fa6c2416205f3c7771f0eb65f4a5c0
ericminio <eric.mignot@gmail.com>
2015-09-08 15:20:10 -0400

    document one way to connect to a socket.io server from a web page

---
+++ web.spec.js
@@ -0,0 +1,68 @@
+describe('web-sockets', function() {
+
+    var io;
+    var app;
+    var server;
+    var serverSocket;
+
+    const Browser = require('zombie');
+    const browser = new Browser();
+
+    beforeEach(function(done) {
+
+        var handler = function(request, response) {
+            response.setHeader('Content-Type', 'text/html');
+            var page = '<html>' +
+            '<body>'+
+                '<script src="/socket.io/socket.io.js"></script>' +
+                '<script>' +
+                    'function connection() {' +
+                        'var socket = io.connect("http://localhost:5000");' +
+                        'socket.on("status", function (data) {' +
+                            'document.getElementById("status").innerHTML = data;' +
+                        '});' +
+                    '};' +
+                    '</script>' +
+                '<button id="connect" onmouseup="connection();">Go!</button>'+
+                '<label id="status">waiting...</label>'+
+            '</body>'+
+            '</html>';
+            response.write(page);
+            response.end();
+        };
+
+        app = require('http').createServer(handler);
+        io = require('socket.io')(app);
+        io.on('connection', function (s) {
+            serverSocket = s
+        });
+
+        app.listen(5000, done);
+    });
+
+    afterEach(function() {
+        io.close();
+        app.close();
+    });
+
+    it('can receive async data from a socket-io server', function(done) {
+
+        browser.visit('http://localhost:5000')
+            .then(function() {
+                return browser.fire('#connect', 'mouseup');
+            })
+            .then(function() {
+                serverSocket.emit('status', 'connected');
+            })
+            .then(function() {
+                setTimeout(function() {
+                    expect(browser.document.getElementById('status').innerHTML).toEqual('connected');
+                    done();
+                }, 100);
+            })
+            .then(function(){}, function(error) {
+                expect(error.message).toEqual(null);
+                done();
+            });
+    });
+});
